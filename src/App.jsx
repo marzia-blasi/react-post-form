bb07d47905406dcbf1d85500a5a3d6a8d6754077
marzia <you@example.com>
2025-07-23 17:55:23 +0200

    fix html

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 function App() {
   const apiPost = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts";
-
-  // const [name, setName] = useState("");
+  /**
+   const [name, setName] = useState("");
   const [formData, setFormData] = useState({
     title: "",
         author: name,
@@ -12,12 +12,12 @@ function App() {
         id: ""
   })
 
- /**
+
   function handleName(e) {
     setName(e.target.value);
     console.log(e.target.value);
   }
- */
+ 
   
   function handleFromData(e) { 
     const fTitle = e.target.title;
@@ -38,13 +38,7 @@ function App() {
     fetch(apiPost, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        title: "",
-        author: name,
-        body: "",
-        public: true,
-        id: "",
-      }),
+      body: JSON.stringify(formData)
     })
       .then((response) => {
         console.log(response.status);
@@ -55,7 +49,7 @@ function App() {
         setName("");
       });
   }
-
+*/
   return (
     <>
       <div className="container">
@@ -67,28 +61,46 @@ function App() {
             type="text"
             className="form-control"
             name="title"
-            
             aria-describedby="helpId"
             placeholder="nome titolo"
-            value={formData.title}
-            onChange={handleFromData}
           />
         </div>
         <input
-            type="text"
+          type="text"
+          className="form-control"
+          name="author"
+          id="author"
+          aria-describedby="helpId"
+          placeholder="nome autore"
+        />
+
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea1" className="form-label">
+            body
+          </label>
+          <textarea
             className="form-control"
-            name="author"
-            id="author"
-            aria-describedby="helpId"
-            placeholder="nome autore"
-            value={name}
-            onChange={handleName}
+            id="exampleFormControlTextarea1"
+            rows={3}
+            defaultValue={""}
           />
-      </div>
-      <div>
-        <button onClick={handleSubmit} className="btn btn-primary">
-          Funzionerà?
-        </button>
+        </div>
+
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="radioDefault"
+            id="radioDefault1"
+          />
+          <label className="form-check-label" htmlFor="radioDefault1">
+            pubblico
+          </label>
+        </div>
+
+        <div>
+          <button className="btn btn-primary">Funzionerà?</button>
+        </div>
       </div>
     </>
   );
